cb58642a9b2bc24111ccedae25b61b69046087a0
nkulung <nkulung334@gmail.com>
2016-06-03 21:08:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="index.css"/>
+	<script src="indexscript.js"></script>
+	<meta charset="UTF-8">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Hege Refsnes">
+	<title>Nick Kulungian</title>
+</head>
+<body onload="pageInd">
+<div class="fillcontainer">
+<div class="container">
+<div id="banner">
+<img src="banner.png" alt="banner" id="imagebanner" />
+<div class="navbar">
+<table>
+	<tr>
+		<td id="homepage"><a href="#">Home</a></td>
+		<td id="aboutpage"><a href="#">About</a></td>
+		<td id="projectspage"><a href="#">Projects</a></td>
+		<td id="contactpage"><a href="#">Contact</a></td>
+	</tr>
+</table>
+
+</div>
+</div>
+<div id="maincontain">
+<div class="feature">
+<h1>Featured Project...</h1>
+<div class="featuredP">
+<?php
+
+
+?>
+</div>
+<br/>
+<br/>
+<br/>
+</div>
+<div class="bodyfill">
+<h2>Nick Kulungian</h2>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<p>Hello I'm Nick Kulungian this is my portfolio containing my
+ work, projects, skills, and experience. I'm currently pursuing a 
+ Bachelors of Science in Computer Science at the University of Rhode Island.</p>
+
+<img id="rightimg"src="uri.png" width="180"/>
+
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
+<!-- IDEAS -->
+
+<!-- 
+Database with projects, 
+project of the day.
+
+-->
